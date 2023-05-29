f2a590cdb37b67a4538a0e5f7f6bd85902a54d73
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-29 16:57:58 +0200

    get elements by api

--- js/main.js
+++ js/main.js
@@ -4,3 +4,64 @@
 // Bonus:
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            email : "",
+            email1 : "",
+            email2:"",
+            email3 :"",
+            email4:"",
+            email5:"",
+            email6:"",
+            email7:"",
+            email8:"",
+            email9:""
+        }
+    },
+    mounted() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email1 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email2 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email3 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email4 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email5 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email6 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email7 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email8 = response.data.response)),
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => 
+                (this.email9 = response.data.response))
+        
+    }
+}).mount('#app');
